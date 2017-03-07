5da9e1ef9639796f9fdb0f7044a9ef20cf1d01d0
Meghan Debity <mdebity@gmail.com>
2017-03-07 17:53:15 -0600

    removed extra file/corrected the over-coding

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
-var treehouseContainer = document.getElementById("treehouseContainer");
-
-function sendToDom(xhrData){
-	var houseString = "";
-	var currentHouse;
-
-	for(var i=0; i<xhrData.myTreehouse.length; i++){
-		currentHouse = xhrData.myTreehouse[i];
-
-		houseString += `<div class="col-sm-6 col-md-4">`;
-    	houseString += `<div class="thumbnail">`;
-        houseString += `<img src="${currentHouse.url}" alt="Meghan's Treehouse Profile">`;
-        houseString += `<div class="caption">`;
-        houseString += `<h3>Treehouse Profile</h3>`;
-        // houseString += `<p></p>`;
-        // houseString += `<p></p>`;
-        houseString += `</div></div></div>`;
-	}
-	
-	treehouseContainer = houseString;	
-}
+var myRequest = new XMLHttpRequest(); 
 
 function executeThisCodeAfterFileLoaded(){
 	var data = JSON.parse(this.responseText);
-	// makeDom(data);
-
+	console.log("myTreehouseData", data);
+	
 }
 
 function executeThisCodeAfterFileFails(){
@@ -31,12 +11,35 @@ function executeThisCodeAfterFileFails(){
 }
 
 
-
-
-
-
-var myRequest = new XMLHttpRequest(); 
 myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
-myRequest.open("GET", "main.json");
+myRequest.open("GET", "https://teamtreehouse.com/meghandebity.json");
 myRequest.send();
+
+
+
+
+
+
+// var treehouseContainer = document.getElementById("treehouseContainer");
+
+// function sendToDom(xhrData){
+// 	var houseString = "";
+// 	var currentHouse;
+
+// 	for(var i=0; i<xhrData.myTreehouse.length; i++){
+// 		currentHouse = xhrData.myTreehouse[i];
+
+// 		houseString += `<div class="col-sm-6 col-md-4">`;
+//     	houseString += `<div class="thumbnail">`;
+//         houseString += `<img src="${currentHouse.url}" alt="Meghan's Treehouse Profile">`;
+//         houseString += `<div class="caption">`;
+//         houseString += `<h3>Treehouse Profile</h3>`;
+//         // houseString += `<p></p>`;
+//         // houseString += `<p></p>`;
+//         houseString += `</div></div></div>`;
+// 	}
+	
+// 	treehouseContainer = houseString;	
+// }
+
